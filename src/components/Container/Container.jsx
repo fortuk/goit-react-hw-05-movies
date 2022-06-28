89c6306ebcbb270b06207ecc63055e40e81f0290
Алексей <fortuk22@rambler.ru>
2022-06-28 03:42:18 +0300

    deploy

--- src/components/Container/Container.jsx
+++
@@ -1,9 +0,0 @@
-import s from './Container.module.css';
-import propTypes from 'prop-types';
-
-export default function Container({ children }) {
-  return <div className={s.container}>{children}</div>;
-}
-Container.propTypes = {
-  children: propTypes.node.isRequired,
-};
--- src/components/Loader/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Spinner from 'react-loader-spinner';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-function Loader() {
-  return <Spinner type="Rings" color="#somecolor" height={80} width={80} />;
-}
-
-export default Loader;
