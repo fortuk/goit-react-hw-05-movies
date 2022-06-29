6289b2f65ec93d5f5a635ff0364dbd29dc533a97
fortuk <88802683+fortuk@users.noreply.github.com>
2022-06-29 21:20:08 +0000

    Deploying to gh-pages from @ fortuk/goit-react-hw-05-movies@bb1037647301b5044d199e100ccc548e3549b040 🚀

--- static/js/not-found-view.894ebb64.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[464],{3566:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)("h1",{children:"Page not found"})}}}]);
-//# sourceMappingURL=not-found-view.894ebb64.chunk.js.map
