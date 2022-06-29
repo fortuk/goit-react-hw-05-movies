448e8daf595c3eaecdad5117e4ff7a9ed87027f2
Алексей <fortuk22@rambler.ru>
2022-06-30 00:05:02 +0300

    delete

--- static/js/not-found-view.58b953aa.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[464],{3566:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var u=t(184);function r(){return(0,u.jsx)("h1",{children:"Page not found"})}}}]);
-//# sourceMappingURL=not-found-view.58b953aa.chunk.js.map
